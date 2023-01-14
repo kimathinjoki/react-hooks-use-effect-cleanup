ca1c30807d9924e8059451f49dcbc2e2b290d809
kimathinjoki <dfkimathinjoki@gmail.com>
2023-01-14 03:43:23 -0800

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,10 +4,19 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+  //   setInterval(() => {
+  //     setTime(new Date());
+  //   }, 1000);
+  // }, []);
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
-  }, []);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  }, []);5
 
   return <div>{time.toString()}</div>;
 }
